1cf9f874a25a52eac30e3ac06817d72e048c494b
Antaryamie <i.harshparmar@gmail.com>
2020-10-07 10:33:07 -0400

    django admin, models, sqlitebrowser, migration,

--- firstproject/firstapp/admin.py
+++ firstproject/firstapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from firstapp.models import Login, Python, Student  
 
 # Register your models here.
+admin.site.register(Login)
+admin.site.register(Python)
+admin.site.register(Student)
--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -3,7 +3,26 @@ from django.db import models
 
 # Create your models here.
 
-class login(models.Model):
+class Login(models.Model):
+    
     name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=40) 
+    email = models.EmailField(max_length=40, unique=False) 
     password = models.CharField(max_length=40)
+    
+    class Meta:
+            verbose_name_plural = "login"
+            
+    def __str__(self):
+        return self.email
+    
+class Python(models.Model):
+    session = models.CharField(max_length=30)
+    def __str__(self):
+        return self.session
+    
+class Student(models.Model):
+    name = models.CharField(max_length=30, unique=False)
+    session =  models.ForeignKey("Python", on_delete = models.PROTECT)
+    
+    def __str__(self):
+        return self.name
