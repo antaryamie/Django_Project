7f2a4db17f90083ebdbe40420c3af5b6b3580119
Antaryamie <i.harshparmar@gmail.com>
2020-10-07 04:36:52 -0400

    first migration

--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -1,3 +1,9 @@
+from os import lseek
 from django.db import models
 
 # Create your models here.
+
+class login(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=40) 
+    password = models.CharField(max_length=40)
